b9700de5bc90ce9663b6949cedd96aaba11a6943
MiaoLeo <42629258+MiaoLeo@users.noreply.github.com>
2023-09-17 23:52:57 +0800

    Add files via upload

--- get_data.m
+++ get_data.m
@@ -3,67 +3,48 @@ function [X,Y]=get_data(data_name)
 
 
 if data_name==1
-    %%Australian
-    load 'D:\MATLABWorkSpace\PHD\High_Dim_FS\data\11_Tumor.mat'
-%     data=Australian;
+    load '..\data\11_Tumor.mat'
+
 
     
 elseif data_name==2
-    %%Breast cancer tissue
-    load 'D:\MATLABWorkSpace\PHD\High_Dim_FS\data\9_Tumor.mat'
-%     data=Breast_cancer_tissue;
+    load '..\data\9_Tumor.mat'
+
           
 elseif data_name==3
-    %%%-Climate
-   load 'D:\MATLABWorkSpace\PHD\High_Dim_FS\data\Brain_Tumor_1.mat'
-%     data=Climate;
+   load '..\data\Brain_Tumor_1.mat'
 
     
 elseif data_name==4
-    %% fri_c0_500_10
-    load 'D:\MATLABWorkSpace\PHD\High_Dim_FS\data\Brain_Tumor_2.mat'
-%     data=fri_c0_500_10;
+    load '..\data\Brain_Tumor_2.mat'
     
   
     
 elseif data_name==5
-    load 'D:\MATLABWorkSpace\PHD\High_Dim_FS\data\DLBCL.mat'
+    load '..\data\DLBCL.mat'
    
 elseif data_name==6
-    %%IonosphereEW
-    load 'D:\MATLABWorkSpace\PHD\High_Dim_FS\data\Leukemia_1.mat'
-%     data=IonosphereEW;   
+    load '..\data\Leukemia_1.mat'
    
 elseif  data_name==7
-    %%Kc1
-    load 'D:\MATLABWorkSpace\PHD\High_Dim_FS\data\Leukemia_2.mat'
-%     data=kc1;
+    load '..\data\Leukemia_2.mat'
    
 elseif data_name==8
-    %%Kc2
-    load 'D:\MATLABWorkSpace\PHD\High_Dim_FS\data\Leukemia_3.mat'
-%     data=kc2;  
+    load '..\data\Leukemia_3.mat'
 
 elseif data_name==9
-    %%Page blocks
-    load 'D:\MATLABWorkSpace\PHD\High_Dim_FS\data\Lung_Cancer.mat'
-%     data=page_blocks;
+    load '..\data\Lung_Cancer.mat'
           
 elseif data_name==10
-    %%%-Parkinsons
-   load 'D:\MATLABWorkSpace\PHD\High_Dim_FS\data\Prostate_Tumor_1.mat'
-%     data=Parkinsons;
+   load '..\data\Prostate_Tumor_1.mat'
 
 
 elseif data_name==11
-    %%%-GLI_85
-    load 'D:\MATLABWorkSpace\PHD\High_Dim_FS\data\GLI_85.mat'
+    load '..\data\GLI_85.mat'
    
 elseif data_name==12
-    %%%-GLI_85
-    load 'D:\MATLABWorkSpace\PHD\High_Dim_FS\data\SMK_CAN_187.mat'
+    load '..\data\SMK_CAN_187.mat'
     
-
 end
 
 
